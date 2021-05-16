1efb22018630a713ecbcebcc426d8b2f2cecd214
taejung2183 <taejung2183@naver.com>
2021-05-16 17:19:41 +0900

    Add competitive programming template

---
+++ template.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define INF 987654321
+#define pb push_back
+#define NL "\n"
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+    cin.tie(0);
+
+//	int C; cin >> C;
+//	while (C--) {
+//	}
+
+//	string s;
+//	getline(cin, s);
+
+//	int n;
+//	while (cin >> n) {
+//	}
+
+	return 0;
+}
+
+//	g++ -std=c++11 -Wall template.cpp -o template
