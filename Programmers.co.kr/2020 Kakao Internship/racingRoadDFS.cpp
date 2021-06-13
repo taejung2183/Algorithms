2df4d4b49eac41520df25c7baaa6258580c4f18a
taejung2183 <taejung2183@naver.com>
2021-06-13 23:25:47 +0900

    DFS with stack

--- Programmers.co.kr/2020 Kakao Internship/racingRoadDFS.cpp
+++ Programmers.co.kr/2020 Kakao Internship/racingRoadDFS.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -14,8 +15,45 @@ int cache[25][25];
 const int dy[4] = {1,-1,0,0};
 const int dx[4] = {0,0,1,-1};
 
-// Complete search
-int dfs(int cy, int cx, int ly, int lx, int cost) {
+struct Car {
+	int cy, cx, ly, lx, cost;
+};
+
+// dfs with stack
+int dfsStack() {
+	stack<Car> s;
+	visited[0][0] = true;
+	s.push({0,0,0,0,0});
+
+	int ret = INF;
+	while (!s.empty()) {
+		Car car = s.top();
+		s.pop();
+
+		// Arrived
+		if (car.cy == len - 1 && car.cx == len - 1) {
+			ret = min(ret, car.cost);
+			break;
+		}
+
+		// Check every direction from current coordinate
+		for (int i = 0; i < 4; ++i) {
+			int ny = car.cy + dy[i];
+			int nx = car.cx + dx[i];
+
+			// Valid boundary
+			if (ny >= 0 && ny < len && nx >= 0 && nx < len && !b[ny][nx] && !visited[ny][nx]) {
+				visited[ny][nx] = true;;
+				int c = (ny != car.ly && nx != car.lx) ? 600 : 100;
+				s.push({ny, nx, car.cy, car.cx, car.cost + c});
+			}
+		}
+	}
+	return ret;
+}
+
+// Recursive dfs
+int dfsRecursion(int cy, int cx, int ly, int lx, int cost) {
 	if (cy == len - 1 && cx == len - 1) return cost;
 
 	int ret = INF;
@@ -27,7 +65,7 @@ int dfs(int cy, int cx, int ly, int lx, int cost) {
 				&& !b[ny][nx] && !visited[ny][nx]) {
 			visited[ny][nx] = 1;
 			int c = (ny != ly && nx != lx) ? 600 : 100;
-			ret = min(ret, dfs(ny, nx, cy, cx, cost + c));
+			ret = min(ret, dfsRecursion(ny, nx, cy, cx, cost + c));
 			visited[ny][nx] = 0;
 		}
 	}
@@ -53,7 +91,7 @@ int dfs(int cy, int cx, int ly, int lx, int cost) {
 // {1, 0, 0, 0, 1}
 // {0, 1, 1, 0, 0}
 // If you start downward, you spend 2100 to get to (3,3)
-// If you start to the right side, you spend 2300 to get to (3,3) so this recurssive task will be pruend.
+// If you start to the right side, you spend 2300 to get to (3,3) so this recurssive task will be pruned.
 // However, the former one spend 3300 to get to the destination,
 // on the other hand, the latter one spend 3000 to get to the destination.
 
@@ -89,9 +127,10 @@ int solution(vector<vector<int>> board) {
 	for (auto& row: visited) for (auto& e: row) e = false;
 	for (auto& row: cost) for (auto& e: row) e = INF;
 
-//	answer = dfs(0, 0, 0, 0, 0);
-	dp(0,0,0,0,0);
-	answer = cost[len - 1][len - 1];
+//	answer = dfsRecursion(0, 0, 0, 0, 0);
+//	dp(0,0,0,0,0);
+//	answer = cost[len - 1][len - 1];
+	answer = dfsStack();
 	
     return answer;
 }
@@ -126,11 +165,19 @@ int main() {
 		{0,1,0,0,0,1},
 		{0,0,0,0,0,0},
 	};
+	vector<vector<int>> board5 = {
+		{0, 0, 0, 0, 0},
+		{0, 1, 1, 1, 0},
+		{0, 0, 1, 0, 0},
+		{1, 0, 0, 0, 1},
+		{0, 1, 1, 0, 0},
+	};
 	cout << solution(board1) << '\n';
 	cout << solution(board2) << '\n';
 	cout << solution(board3) << '\n';
 	cout << solution(board4) << '\n';
+	cout << solution(board5) << '\n';
 	return 0;
 }
 
-// g++ -std=c++11 -Wall racingRoad.cpp -o racingRoad
+// g++ -std=c++11 -Wall racingRoadDFS.cpp -o racingRoadDFS
