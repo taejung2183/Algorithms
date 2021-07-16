d21aae20625ba1bf6bdb202038ccb36360310917
taejung2183 <taejung2183@naver.com>
2021-07-16 14:24:11 +0900

    Initialize array with 0 at the beginning and passed

--- Programmers.co.kr/2021 Kakao Blind Recruitment/광고삽입.cpp
+++ Programmers.co.kr/2021 Kakao Blind Recruitment/광고삽입.cpp
@@ -27,10 +27,11 @@ string toTimeFormat(int sec) {
 
 string solution(string play_time, string adv_time, vector<string> logs) {
     string answer = "";
-	int watchCnt[max_sec];
+	// Don't forget to initialize array
+	long long watchCnt[max_sec] = {0, };
 	int playSec = toSecond(play_time);
 	int advSec = toSecond(adv_time);
-	
+
 	for (auto& e: logs) {
 		int start = toSecond(e.substr(0,8));
 		int end = toSecond(e.substr(9,8));
@@ -38,18 +39,15 @@ string solution(string play_time, string adv_time, vector<string> logs) {
 		watchCnt[end] -= 1;
 	}
 
-	for (int i = 1; i <= max_sec; ++i) watchCnt[i] += watchCnt[i - 1];
-	for (int i = 1; i <= max_sec; ++i) watchCnt[i] += watchCnt[i - 1];
+	for (int i = 1; i < playSec; ++i) watchCnt[i] += watchCnt[i - 1];
+	for (int i = 1; i < playSec; ++i) watchCnt[i] += watchCnt[i - 1];
 
-	// 자료형 문제인가..? 매번 실행마다 결과가 다름
 	long long ans = 0, maxPlayed = watchCnt[advSec - 1];
-//	cout << "accumulated time: " << maxPlayed << " started at: " << ans <<  '\n';
-	for (int i = 1; i + advSec <= playSec; ++i) {
-		long long tmp = watchCnt[i + advSec - 1] - watchCnt[i - 1]; 
+	for (int i = advSec; i < playSec; ++i) {
+		long long tmp = watchCnt[i] - watchCnt[i - advSec]; 
 		if (maxPlayed < tmp) {
 			maxPlayed = tmp;
-			ans = i;
-//			cout << "accumulated time: " << tmp << " started at: " << i <<  '\n';
+			ans = i - advSec + 1;
 		}
 	}
 	answer = toTimeFormat(ans);
@@ -57,34 +55,16 @@ string solution(string play_time, string adv_time, vector<string> logs) {
 }
 
 int main() {
-//	string play_time = "02:03:55";
-//	string adv_time = "00:14:15";
-//	vector<string> logs = {
-//		"01:20:15-01:45:14",
-//		"00:40:31-01:00:00",
-//		"00:25:50-00:48:29",
-//		"01:30:59-01:53:29",
-//		"01:37:44-02:02:30",
-//	};
-	string play_time = "99:59:59";
-	string adv_time = "25:00:00";	
-	vector<string> logs = {
-		"69:59:59-89:59:59", 
-		"01:00:00-21:00:00", 
-		"79:59:59-99:59:59", 
-		"11:00:00-31:00:00"	
+	string p1 = "02:03:55";
+	string ad1 = "00:14:15";
+	vector<string> logs1 = {
+		"01:20:15-01:45:14",
+		"00:40:31-01:00:00",
+		"00:25:50-00:48:29",
+		"01:30:59-01:53:29",
+		"01:37:44-02:02:30",
 	};
-//	string play_time = "00:00:13";
-//	string adv_time = "00:00:04";	
-//	vector<string> logs = {
-//		"00:00:00-00:00:02",
-//		"00:00:01-00:00:03",
-//		"00:00:02-00:00:04",
-//		"00:00:08-00:00:12",
-//		"00:00:09-00:00:12",
-//		"00:00:11-00:00:13",
-//	};
-	cout << solution(play_time, adv_time, logs) << '\n';
+	cout << solution(p1,ad1,logs1) << '\n';
 	return 0;
 }
 
